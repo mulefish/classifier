c03716e218ebcd66ac5e1e767fa79083033f7742
mulefish <finch.wren.swift@gmail.com>
2020-02-10 15:57:53 -0800

    1st pass done

--- kanji.js
+++ kanji.js
@@ -1,239 +1,143 @@
-let canvas, ctx, flag = false,
-
-prevX = 0,
-
-currX = 0,
-
-prevY = 0,
-
-currY = 0,
-
-dot_flag = false;
-
-
-
-const offsetX = undefined;
-
-const offsetY = undefined;
-
-
-
-let x = "black",
-
-y = 2;
-
-
-
+let canvas;
+let ctx;
+let flag = false;
+let prevX = 0;
+let currX = 0;
+let prevY = 0;
+let currY = 0;
+let dot_flag = false;
+let thickness = 3; // line thickness
 function init() {
-
-alert("init! 2 ")
-
-canvas = document.getElementById('can');
-
-ctx = canvas.getContext("2d");
-
-
-
-
-
-w = canvas.width;
-
-h = canvas.height;
-
+    canvas = document.getElementById('can');
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+    canvas.addEventListener("touchmove", function(e) {
+        findxy('touchmove', e)
+    }, false);
+    canvas.addEventListener("touchstart", function(e) {
+        findxy('touchstart', e)
+    }, false);
+    canvas.addEventListener("touchend", function(e) {
+        findxy('touchend', e)
+    }, false);
 
 
-canvas.addEventListener("mousemove", function (e) {
+    canvas.addEventListener("mousemove", function(e) {
+        findxy('move', e)
+    }, false);
 
-    findxy('move', e)
 
-}, false);
 
-canvas.addEventListener("mousedown", function (e) {
 
-    findxy('down', e)
+    canvas.addEventListener("mousedown", function(e) {
+        findxy('down', e)
+    }, false);
 
-}, false);
 
-canvas.addEventListener("mouseup", function (e) {
 
-    findxy('up', e)
 
-}, false);
+    canvas.addEventListener("mouseup", function(e) {
 
-canvas.addEventListener("mouseout", function (e) {
+        findxy('up', e)
 
-    findxy('out', e)
+    }, false);
 
-}, false);
+    canvas.addEventListener("mouseout", function(e) {
+
+        findxy('out', e)
+
+    }, false);
 
 }
-
-
-
-// function color(obj) {
-
-//     switch (obj.id) {
-
-//         case "green":
-
-//             x = "green";
-
-//             break;
-
-//         case "blue":
-
-//             x = "blue";
-
-//             break;
-
-//         case "red":
-
-//             x = "red";
-
-//             break;
-
-//         case "yellow":
-
-//             x = "yellow";
-
-//             break;
-
-//         case "orange":
-
-//             x = "orange";
-
-//             break;
-
-//         case "black":
-
-//             x = "black";
-
-//             break;
-
-//         case "white":
-
-//             x = "white";
-
-//             break;
-
-//     }
-
-//     if (x == "white") y = 14;
-
-//     else y = 2;
-
-
-
-// }
-
 
 
 function draw() {
-
-ctx.beginPath();
-
-ctx.moveTo(prevX, prevY);
-
-ctx.lineTo(currX, currY);
-
-ctx.strokeStyle = x;
-
-ctx.lineWidth = y;
-
-ctx.stroke();
-
-ctx.closePath();
-
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = thickness;
+    ctx.stroke();
+    ctx.closePath();
 }
-
-
 
 function erase() {
-
-var m = confirm("Want to clear");
-
-if (m) {
-
-    ctx.clearRect(0, 0, w, h);
-
-    document.getElementById("canvasimg").style.display = "none";
-
+    // var m = confirm("Want to clear");
+    // if (m) {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("canvasimg").style.display = "none";
+    // }
 }
-
-}
-
-
 
 function save() {
-
-document.getElementById("canvasimg").style.border = "2px solid";
-
-var dataURL = canvas.toDataURL();
-
-document.getElementById("canvasimg").src = dataURL;
-
-document.getElementById("canvasimg").style.display = "inline";
-
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
 }
-
-
 
 function findxy(res, e) {
-
-if (res == 'down') {
-
-    prevX = currX;
-
-    prevY = currY;
-
-    currX = e.clientX - canvas.offsetLeft;
-
-    currY = e.clientY - canvas.offsetTop;
-
-
-
-    flag = true;
-
-    dot_flag = true;
-
-    if (dot_flag) {
-
-        ctx.beginPath();
-
-        ctx.fillStyle = x;
-
-        ctx.fillRect(currX, currY, 2, 2);
-
-        ctx.closePath();
-
-        dot_flag = false;
-
-    }
-
-}
-
-if (res == 'up' || res == "out") {
-
-    flag = false;
-
-}
-
-if (res == 'move') {
-
-    if (flag) {
-
+    if (res == 'down' ) {
         prevX = currX;
-
         prevY = currY;
-
         currX = e.clientX - canvas.offsetLeft;
-
         currY = e.clientY - canvas.offsetTop;
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = "black";
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if ( res === "touchstart") {
+  
+    prevX = currX;
+    prevY = currY;
+    currX = e.touches[0].clientX - canvas.offsetLeft;
+    currY = e.touches[0].clientY - canvas.offsetTop;
+    flag = true;
+    dot_flag = true;
+    if (dot_flag) {
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.fillRect(currX, currY, 2, 2);
+        ctx.closePath();
+        dot_flag = false;
+    }
 
-        draw();
+
+
 
     }
 
-}
 
+
+    if (res == 'up' || res == "out" || res === 'touchend') {
+        flag = false;
+    }
+
+    if (res == 'touchmove') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.touches[0].clientX - canvas.offsetLeft;
+            currY = e.touches[0].clientY - canvas.offsetTop;
+            draw();
+        }
+    }
+
+    
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
 }
